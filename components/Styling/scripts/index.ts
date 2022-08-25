ebc21bb735f4572599ca9aeb9388b05bdc77b334
Leo Dion <leogdion@brightdigit.com>
2022-08-25 18:44:40 +0000

    Analytics

--- components/Styling/scripts/index.ts
+++ components/Styling/scripts/index.ts
@@ -1,13 +1,37 @@
-import './../styles/styles.css';
+import "./../styles/styles.css";
 
-// declare global {
-//     interface Window {
-//       gtag: (...args: any[]) => void
-//       dataLayer: Record<string, any>
-//     }
-//   }
+import Plausible from "plausible-tracker";
 
-// window.dataLayer = window.dataLayer || [];
-// function gtag(){window.dataLayer.push(arguments);}
-// gtag('js', new Date());
-// gtag('config', 'G-K3MSJ0CTMJ');
+const pageID = "7d44e5cdd9bf0241d295591162f4c69ac1de5e09";
+
+const plausible = Plausible({
+  domain: "getbushel.app",
+});
+
+window.addEventListener("load", () => {
+  plausible.trackPageview();
+  document.querySelectorAll("form.signup").forEach((element) => {
+    const formElement = element as HTMLFormElement;
+    const propsString =
+      element.getAttribute("data-props")?.replaceAll("'", '"') ?? "{}";
+    const props = JSON.parse(propsString) || {};
+    props.pageID = pageID;
+    if (formElement) {
+      formElement.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        plausible.trackEvent("signup", {
+          callback: () => {
+            formElement.submit();
+          },
+          props,
+        });
+        setTimeout(() => {
+          formElement.submit();
+        }, 500);
+      });
+    }
+  });
+});
+
+document.body.style.visibility = "visible";
